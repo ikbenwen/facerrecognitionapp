7678cd33bf5cb9096edf28c3294f72d7c5f1969d
ikbenwen <w.michels@novi-education.nl>
2021-01-16 11:48:55 +0100

    added .idea to gitignore

--- src/App.js
+++ src/App.js
@@ -1,37 +1,115 @@
-import React, {Component, Fragment} from 'react';
-import Navigation from './components/Navigation/Navigation';
+import React, {Component} from 'react';
+import Particles from "react-particles-js";
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from "./components/Register/Register";
 import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from "react-particles-js";
 import './App.css';
 
-const particalOptions = {
 
+
+const app = new Clarifai.App({
+    apiKey: '96f44b7595d4401d85f223c00cd3c633'
+});
+
+const particlesOptions = {
     particles: {
-        line_linked: {
-            shadow: {
+        number: {
+            value: 30,
+            density: {
                 enable: true,
-                color: "#3CA9D1",
-                blur: 5
+                value_area: 200
             }
         }
     }
 }
 
-function App() {
-  return (
-    <div className="App">
-        <Particles
-            params={particalOptions}
-        />
-<Navigation />
-<Logo />
-<Rank />
-<ImageLinkForm />
-{/*<FaceRecognition />*/}
-    </div>
-  );
-}
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route:'signin',
+            isSignedIn: 'false'
+        }
+    };
 
+    calculateFaceLocation = (data) => {
+       const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+       const image = document.getElementById('inputimage');
+       const width = Number(image.width);
+       const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        };
+    }
+
+    displayFaceBox = (box) => {
+        console.log(box)
+        this.setState({box: box});
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models
+            .predict(
+            Clarifai.FACE_DETECT_MODEL,
+                this.state.input)
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+                .catch(err => console.log(err))
+    }
+
+    onRouteChange = (route) => {
+        if (route === 'signout') {
+            this.setState({isSignedIn:false})
+        } else if (route === 'home') {
+            this.setState({isSignedIn: 'true'})
+        }
+        this.setState({ route: route} );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Particles className='particles'
+                           params={particlesOptions}
+                />
+                <Navigation onRouteChange={this.onRouteChange} />
+                { this.state.route === 'home'
+                    ?  <div>
+                        <Logo />
+                        <Rank />
+                        <ImageLinkForm
+                            onInputChange={this.onInputChange}
+                            onButtonSubmit={this.onButtonSubmit}
+                        />
+                        <FaceRecognition
+                            box={this.state.box}
+                            imageUrl={this.state.imageUrl}
+                        />
+                    </div>
+                    : (
+                        this.state.route === 'signin'
+                        ? <Signin onRouteChange={this.onRouteChange}/>
+                        : <Register onRouteChange={this.onRouteChange}/>
+                    )
+
+                }
+            </div>
+        );
+    }
+}
 export default App;
