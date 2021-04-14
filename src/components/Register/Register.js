aa78ac31971f33c0c0ae8bb2b665d86b68c68af1
ikbenwen <w.michels@novi-education.nl>
2021-04-14 15:43:55 +0200

    deploy

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://fast-brushlands-61934.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
